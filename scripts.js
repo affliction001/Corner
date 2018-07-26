d2d9b4369919a76917569f419cac755a689c88f8
Vladislav Semenov <affliction001@mail.ru>
2018-07-26 21:29:05 +0300

    For job

--- scripts.js
+++ scripts.js
@@ -5,8 +5,8 @@ const button = document.querySelector('#button');
 button.addEventListener('click', function(e) {
 	const radius = document.querySelector('#R').value;
 	const corner = document.querySelector('#v').value;
+	
+	const leng = 2 * radius * Math.sin((corner/2 * Math.PI / 180));
 
-	const leng = +radius * Math.sqrt(2 - (2 / Math.cos(+corner)));
-
-	document.querySelector('.output').textContent = leng;
+	document.querySelector('.output').textContent = Math.round(leng * 10) / 10;
 });
